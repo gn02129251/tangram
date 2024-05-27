ec135f02528fe93ddd478fa2ac0d94ab49ae6630
yiju_wu <yiju_wu@brogent.com>
2024-05-27 22:04:51 +0800

    chore: Create gamma.py for image gamma processing

---
+++ tangram/gamma.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon 27 May 2024
+
+Author: YIJU WU (gn02129251)
+
+File: gamma.py
+
+Topic: A module developed for image gamma processing
+
+"""
+
+
+def main(*args):
+    return 0
+
+
+if __name__ == "__main__":
+    main()
